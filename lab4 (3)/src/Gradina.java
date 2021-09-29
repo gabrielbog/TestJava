0118fd118bce4216a2a75e08430530d61cc27f96
jinxgtaf <63867581+jinxgtaf@users.noreply.github.com>
2021-09-29 11:33:14 +0300

    wow

--- lab4 (3)/src/Gradina.java
+++ lab4 (3)/src/Gradina.java
@@ -1,2 +1,35 @@
-public class Gradina {
+public class Gradina
+{
+    //campuri
+    private String numeGradina;
+    private int adresaGradina;
+    public String recenzieGradina;
+    public int notaGradina;
+
+    Planta planta1 = new Planta();
+
+    //constructori
+
+    //metode
+    public String getNumeGradina()
+    {
+        return numeGradina;
+    }
+
+    public void setNumeGradina(String numeGradina)
+    {
+        this.numeGradina = numeGradina;
+    }
+
+    public int getAdresaGradina()
+    {
+        return adresaGradina;
+    }
+
+    public void setAdresaGradina(int adresaGradina)
+    {
+        this.adresaGradina = adresaGradina;
+    }
+
+
 }
--- lab4 (3)/src/Planta.java
+++ lab4 (3)/src/Planta.java
@@ -1,2 +1,88 @@
-public class Planta {
+public class Planta
+{
+    //campuri
+    public int raritatePlanta;
+    public String notitePlanta;
+    private int idPlanta;
+    private String numePlanta;
+
+    //constructori
+    public Planta()
+    {
+        numePlanta = null;
+        idPlanta = 0;
+        notitePlanta = null;
+        raritatePlanta = 0;
+    }
+
+    public Planta(String numePlanta, int idPlanta)
+    {
+        this.numePlanta = numePlanta;
+        this.idPlanta = idPlanta;
+        notitePlanta = null;
+        raritatePlanta = 1;
+    }
+
+    public Planta(String numePlanta, int idPlanta, String notitePlanta)
+    {
+        this.numePlanta = numePlanta;
+        this.idPlanta = idPlanta;
+        this.notitePlanta = notitePlanta;
+        raritatePlanta = 1;
+    }
+
+    public Planta(String numePlanta, int idPlanta, String notitePlanta, int raritatePlanta)
+    {
+        this.numePlanta = numePlanta;
+        this.idPlanta = idPlanta;
+        this.notitePlanta = notitePlanta;
+        this.raritatePlanta = raritatePlanta;
+    }
+
+    //metode
+    public int getIdPlanta()
+    {
+        return idPlanta;
+    }
+
+    public void setIdPlanta(int idPlanta)
+    {
+        this.idPlanta = idPlanta;
+    }
+
+    public String getNumePlanta()
+    {
+        return numePlanta;
+    }
+
+    public void setNumePlanta(String numePlanta)
+    {
+        this.numePlanta = numePlanta;
+    }
+
+    public int ratingCorectPlanta()
+    {
+        if(raritatePlanta <= 1 || raritatePlanta >= 11)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public String parerePlanta()
+    {
+        if(raritatePlanta >= 1 && raritatePlanta <= 3)
+            return "COMMON";
+        else if(raritatePlanta >= 4 && raritatePlanta <= 6)
+            return "RELATIVELY COMMON";
+        else if(raritatePlanta >= 7 && raritatePlanta <= 9)
+            return "RARE";
+        else if(raritatePlanta == 10)
+            return "VERY RARE";
+        else
+            return "RARITATE TREBUIE SA SE AFLE PE SCARA 1-10";
+    }
 }
